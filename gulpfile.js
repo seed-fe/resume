d25689540016477192f193227598d0ff86b49325
seed-fe <zzlfullstack@gmail.com>
2016-12-16 13:40:04 +0800

    gulp改进工作流
    通过gulp改进工作流分成了开发阶段和发布阶段

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 // 调用 .create() 意味着你得到一个唯一的实例并允许您创建多个服务器或代理。
 var browserSync = require('browser-sync').create();
+var ghPages = require('gulp-gh-pages');
 // 执行gulp命令就会执行default任务
 gulp.task('default', function() {
 	console.log('hello gulp');
@@ -10,10 +11,10 @@ gulp.task('watch', function() {
 	// 启动Browsersync服务。这将启动一个服务器，代理服务器（proxy）或静态服务器（server）
 	browserSync.init({
 		// 设置监听的文件，以gulpfile.js所在的根目录为起点，单个文件就用字符串，多个文件就用数组
-		files: ["*.html", "css/*.css", "js/*.js"],
+		files: ["src/*.html", "src/css/*.css", "src/image/*.*"],
 		// ，这里是静态服务器，监听3000端口
 		server: {
-			baseDir: "./"
+			baseDir: "./src"
 		},
 		// 在不同浏览器上镜像点击、滚动和表单，即所有浏览器都会同步
 		ghostMode: {
@@ -28,3 +29,8 @@ gulp.task('watch', function() {
 		port: 8090
 	});
 });
+// 提示failed to find git repository in undefined
+gulp.task('deploy', function() {
+	return gulp.src('./**/*')
+		.pipe(ghPages());
+})
